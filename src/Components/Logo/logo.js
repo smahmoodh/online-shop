8ab1c15d1571720bcf986f8c68f1eafa78920030
S.M.H <seyedmahmoodhosseini@gmail.com>
2023-03-28 18:42:29 +0430

    change types

---
+++ src/Components/Logo/logo.js
@@ -0,0 +1,7 @@
+import logo from "../../Assets/img/logo-1.png";
+import './logo.css';
+export const Logo = (props) => (
+    <div  className={props.cssClass}>
+        <img src={logo} />
+    </div>
+)
